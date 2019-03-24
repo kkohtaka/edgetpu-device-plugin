6e73d475ad672181e15f98b06c792c9e13af9e16
Kazumasa Kohtaka <kkohtaka@gmail.com>
2019-03-24 18:35:12 +0900

    Overwrite platform recognizer for building sample Docker image

---
+++ samples/platform_recognizer.amd64.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source ./colors.sh
+
+platform="x86_64_linux"
+echo -e "${GREEN}Recognized as Linux on x86_64!${DEFAULT}"
---
+++ samples/platform_recognizer.arm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source ./colors.sh
+
+platform="raspberry_pi_3b"
+echo -e "${GREEN}Recognized as Raspberry Pi 3 B!${DEFAULT}"
